6c349dc2341b1f3a62b6f85173025b3119fa6652
Ruslan Naumov <rusnaumoff@gmail.com>
2023-09-14 17:26:08 +0300

    add audio

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ import requests
 
 load_dotenv()
 TOKEN =  os.getenv('botToken')
-
+git_url = os.getenv('gitURL')
 bot = telebot.TeleBot(token=TOKEN)
 
 class GetImageAudio():
     api_image = "https://api.thecatapi.com/v1/images/search"
-    
+
     
     @classmethod
     def get_image(cls):
@@ -29,7 +29,7 @@ class GetImageAudio():
 @bot.message_handler(commands=["start"])
 def greeting(message: telebot.types.Message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.add("get photo", "get audio")
+    keyboard.add("get photo", "get audio", "get repo")
     bot.send_message(
         chat_id=message.from_user.id,
         text="Привет! Я могу высылать изображения, аудио и отправлять ссылку на git.",
@@ -46,8 +46,13 @@ def get_media(message: telebot.types.Message):
         )
     elif message.text == "get audio":
         bot.send_audio(
+            chat_id=message.from_user.id, 
+            audio="https://github.com/A1kawa1/oop_laba1/blob/main/audio/Ludovico_Einaudi_Fly.mp3"
+        )
+    elif message.text == "get repo":
+        bot.send_message(
             chat_id=message.from_user.id,
-            audio=
+            text=git_url
         )
 
 bot.infinity_polling(timeout=600)
